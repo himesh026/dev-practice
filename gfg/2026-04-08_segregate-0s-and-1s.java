ada02b4876611d3a3e693d12fdd4862edc52e1f5
himesh026 <himeshdhaka616@gmail.com>
2026-04-08 15:07:03 +0530

    practice: Segregate 0s and 1s solution

---
+++ gfg/2026-04-08_segregate-0s-and-1s.java
@@ -0,0 +1,12 @@
+// Problem   : Segregate 0s and 1s
+// Difficulty: Easy
+// Tags      : [object Object]
+// Language  : Java
+// Date      : 2026-04-08
+// ───────────────────────────────────────────────────────
+// Time complexity: O(N)
+// Space complexity: O(1)
+class Solution {
+
+    // Method to segregate 0s and 1s in an array using a two-pointer approach.
+    // All 0s will be moved to the left
