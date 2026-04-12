6dca0258abad3e04007a59a471b5b37981941494
himesh026 <himeshdhaka616@gmail.com>
2026-04-12 19:56:57 +0530

    feat: solve Toeplitz matrix

---
+++ gfg/2026-04-12_toeplitz-matrix.js
@@ -0,0 +1,10 @@
+// Problem   : Toeplitz matrix
+// Difficulty: Easy
+// Tags      : [object Object]
+// Language  : JavaScript
+// Date      : 2026-04-12
+// ───────────────────────────────────────────────────────
+// Time complexity: O(R * C) where R is the number of rows and C is the number of columns in the matrix.
+// Space complexity: O(1)
+const isToeplitzMatrix = (matrix) => {
+    // Handle edge cases for empty or invalid
