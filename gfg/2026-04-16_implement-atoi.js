e359a14a02a4ef4800d01650c79ff90e2c2b8241
himesh026 <himeshdhaka616@gmail.com>
2026-04-16 20:46:31 +0530

    practice: Implement Atoi solution

---
+++ gfg/2026-04-16_implement-atoi.js
@@ -0,0 +1,11 @@
+// Problem   : Implement Atoi
+// Difficulty: Medium
+// Tags      : [object Object]
+// Language  : JavaScript
+// Date      : 2026-04-16
+// ───────────────────────────────────────────────────────
+// Time complexity: O(L) where L is the length of the input string.
+// Space complexity: O(1)
+const myAtoi = (s) => {
+    let i = 0; // Pointer to the current character in the string
+    let sign
