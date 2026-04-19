b9028c8b35190d0720a37e0cdb11b6b1ef18b85e
himesh026 <himeshdhaka616@gmail.com>
2026-04-19 14:39:20 +0530

    add: Check for Power (DSA)

---
+++ gfg/2026-04-19_check-for-power.py
@@ -0,0 +1,13 @@
+# Problem   : Check for Power
+# Difficulty: Basic
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-04-19
+# ───────────────────────────────────────────────────────
+# O(log n) - for checking power of any integer base, or O(1) for power of 2
+# O(1) - space complexity
+
+import math
+
+def is_power_of_two(n: int) -> bool:
+    """
