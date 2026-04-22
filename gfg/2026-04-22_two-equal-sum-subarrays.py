82d0de6a213cd8a0c8875140aae8d0ba29269729
himesh026 <himeshdhaka616@gmail.com>
2026-04-23 02:35:25 +0530

    feat: solve Two Equal Sum Subarrays

---
+++ gfg/2026-04-22_two-equal-sum-subarrays.py
@@ -0,0 +1,13 @@
+# Problem   : Two Equal Sum Subarrays
+# Difficulty: Easy
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-04-22
+# ───────────────────────────────────────────────────────
+import collections
+
+# Time complexity: O(N^2)
+# Space complexity: O(N^2)
+def two_equal_sum_subarrays(arr: list[int]) -> bool:
+    """
+    Determines if there exist two non-empty,
