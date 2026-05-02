a1495b55ae33acdc7dde7fc93d3ff525c57cf844
himesh026 <himeshdhaka616@gmail.com>
2026-05-03 02:24:28 +0530

    add: Sort by Set Bit Count (DSA)

---
+++ gfg/2026-05-02_sort-by-set-bit-count.py
@@ -0,0 +1,8 @@
+# Problem   : Sort by Set Bit Count
+# Difficulty: Easy
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-05-02
+# ───────────────────────────────────────────────────────
+# O(N log N * log M) where N is the number of elements and M is the maximum value in the list
+# O(N + log M) where N is the number of elements (for Timsort auxiliary space) and log M (for string conversion)
