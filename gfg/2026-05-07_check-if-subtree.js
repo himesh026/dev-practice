a0603fb7e01280392e3a815ce353271b9b8a2bff
himesh026 <himeshdhaka616@gmail.com>
2026-05-07 16:06:32 +0530

    practice: Check if subtree solution

---
+++ gfg/2026-05-07_check-if-subtree.js
@@ -0,0 +1,8 @@
+// Problem   : Check if subtree
+// Difficulty: Medium
+// Tags      : [object Object]
+// Language  : JavaScript
+// Date      : 2026-05-07
+// ───────────────────────────────────────────────────────
+// Time complexity: O(N * M), where N is the number of nodes in the main tree and M is the number of nodes in the subtree.
+// Space complexity: O(H1 + H2), where H1 is the height of the main tree and H2 is
