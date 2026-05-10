3a8589882eede3922b90e993b8c9c984e859e0b3
himesh026 <himeshdhaka616@gmail.com>
2026-05-11 02:29:32 +0530

    practice: Palindrome Pairs solution

---
+++ gfg/2026-05-10_palindrome-pairs.py
@@ -0,0 +1,9 @@
+# Problem   : Palindrome Pairs
+# Difficulty: Hard
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-05-10
+# ───────────────────────────────────────────────────────
+# O(N * L^2) where N is the number of words, L is the maximum length of a word
+# O(N * L) for hash map and O(N * L^2) for results in worst case
+def palindrome_pairs(words
