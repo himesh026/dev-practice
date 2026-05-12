23012b5c16f14d53d9b117ba6bcd904da286f34f
himesh026 <himeshdhaka616@gmail.com>
2026-05-12 21:37:56 +0530

    add: Range LCM Queries (DSA)

---
+++ gfg/2026-05-12_range-lcm-queries.py
@@ -0,0 +1,11 @@
+# Problem   : Range LCM Queries
+# Difficulty: Medium
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-05-12
+# ───────────────────────────────────────────────────────
+# O(N log A + Q log N log A) where A is max value in array, or O(N + Q log N) if GCD/LCM are O(1) for Python's arbitrary precision integers.
+# O(N)
+import math
+
+#
