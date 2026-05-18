71a8a4b0fcf27dc1326b8fbc2c655fd89a156a41
himesh026 <himeshdhaka616@gmail.com>
2026-05-18 17:24:53 +0530

    feat: create API utility

---
+++ backend/2026-05-18_service.js
@@ -0,0 +1,10 @@
+// Type      : Backend Utility
+// Date      : 2026-05-18
+// ───────────────────────────────────────────────────────
+/**
+ * @typedef {import('socket.io').Server} SocketIOServer
+ * @typedef {import('socket.io').Socket} SocketIOSocket
+ */
+
+/**
+ * A map to store online users per room: Map<roomName, Set<socketId
