69ffe5e1b0ad86f62f6e70844518e574516d9e02
himesh026 <himeshdhaka616@gmail.com>
2026-05-23 02:49:08 +0530

    feat: solve Transform to Sum Tree

---
+++ gfg/2026-05-22_transform-to-sum-tree.js
@@ -0,0 +1,8 @@
+// Problem   : Transform to Sum Tree
+// Difficulty: Easy
+// Tags      : [object Object]
+// Language  : JavaScript
+// Date      : 2026-05-22
+// ───────────────────────────────────────────────────────
+// Time complexity: O(N), where N is the number of nodes in the tree. Each node is visited once.
+// Space complexity: O(H), where H is the height of the tree. This is due to the recursion stack. In the worst case (skew
