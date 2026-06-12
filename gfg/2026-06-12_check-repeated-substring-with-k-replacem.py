97eb3fa76d0c7c6edf8e90edbae0a94a4f73d782
himesh026 <himeshdhaka616@gmail.com>
2026-06-12 17:24:29 +0530

    add: Check Repeated Substring with K Rep (DSA)

---
+++ gfg/2026-06-12_check-repeated-substring-with-k-replacem.py
@@ -0,0 +1,10 @@
+# Problem   : Check Repeated Substring with K Replacements
+# Difficulty: Medium
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-06-12
+# ───────────────────────────────────────────────────────
+# O(N * sqrt(N) + N * D) where N is string length, D is number of divisors of N. In worst case D can be N.
+# O(1)
+
+def check_repeated_substring_with_k_replacements(s
