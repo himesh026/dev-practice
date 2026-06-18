12171778aadd244d02ddca3fa1e0fe40a9b312dd
himesh026 <himeshdhaka616@gmail.com>
2026-06-18 17:29:10 +0530

    feat: solve Coverage of all Zeros in a Binary M

---
+++ gfg/2026-06-18_coverage-of-all-zeros-in-a-binary-matrix.java
@@ -0,0 +1,11 @@
+// Problem   : Coverage of all Zeros in a Binary Matrix
+// Difficulty: Easy
+// Tags      : [object Object]
+// Language  : Java
+// Date      : 2026-06-18
+// ───────────────────────────────────────────────────────
+// Time complexity: O(M*N) where M is the number of rows and N is the number of columns in the matrix.
+// Space complexity: O(1)
+class Solution {
+    public int coverageOfAllZeros(int[][] matrix) {
+        if (matrix
