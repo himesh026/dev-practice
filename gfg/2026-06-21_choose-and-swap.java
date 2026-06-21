9dce7b2ed915466d89cafb8ed9761806d9b6b589
himesh026 <himeshdhaka616@gmail.com>
2026-06-21 16:55:55 +0530

    feat: solve Choose and Swap

---
+++ gfg/2026-06-21_choose-and-swap.java
@@ -0,0 +1,11 @@
+// Problem   : Choose and Swap
+// Difficulty: Medium
+// Tags      : [object Object]
+// Language  : Java
+// Date      : 2026-06-21
+// ───────────────────────────────────────────────────────
+char[] sArr = s.toCharArray(); // Convert string to char array for modification
+
+        // Calculate the smallest character from current position to the end of the string
+        char[] minCharSuffix = new char[sArr.length];
+        minCharSuffix[sArr.
