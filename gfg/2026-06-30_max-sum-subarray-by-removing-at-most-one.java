152322e2d852913e658a9bbc2d7734f75b63c955
himesh026 <himeshdhaka616@gmail.com>
2026-07-01 03:13:22 +0530

    practice: Max sum subarray by removing at mos solution

---
+++ gfg/2026-06-30_max-sum-subarray-by-removing-at-most-one.java
@@ -0,0 +1,15 @@
+// Problem   : Max sum subarray by removing at most one element
+// Difficulty: Medium
+// Tags      : [object Object]
+// Language  : Java
+// Date      : 2026-06-30
+// ───────────────────────────────────────────────────────
+// Time complexity: O(N)
+// Space complexity: O(N)
+import java.util.Arrays;
+
+class Solution {
+
+    public int maxSumSubarrayRemovingOneElement(int[] nums) {
+        int n = nums.length;
+        if (n ==
