67af888b61397d8b79178f1c587c0cc9acf1cd05
himesh026 <himeshdhaka616@gmail.com>
2026-07-03 21:14:11 +0530

    feat: solve Ways to Increase LCS by One

---
+++ gfg/2026-07-03_ways-to-increase-lcs-by-one.py
@@ -0,0 +1,14 @@
+# Problem   : Ways to Increase LCS by One
+# Difficulty: Medium
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-07-03
+# ───────────────────────────────────────────────────────
+import collections
+
+# Time complexity: O(N * M + N * ALPHA * M) where ALPHA is alphabet size (26)
+# Space complexity: O(N * M)
+
+def solve():
+    s1 = input()
+    s2 = input()
