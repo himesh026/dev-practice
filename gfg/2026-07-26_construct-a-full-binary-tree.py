f5d86543013c01004ccf06b96ad05b7ef9341b81
himesh026 <himeshdhaka616@gmail.com>
2026-07-27 02:36:34 +0530

    feat: solve Construct a Full Binary Tree

---
+++ gfg/2026-07-26_construct-a-full-binary-tree.py
@@ -0,0 +1,8 @@
+# Problem   : Construct a Full Binary Tree
+# Difficulty: Medium
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-07-26
+# ───────────────────────────────────────────────────────
+# Time complexity: O(N * C(N/2)), where N is the number of nodes and C(k) is the k-th Catalan number.
+# Space complexity: O(N * C(N/2) * N) for storing all trees, or O
