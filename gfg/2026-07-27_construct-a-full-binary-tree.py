4b0bc477ce7a9de2ca8495a8dbcc7e64ed610322
himesh026 <himeshdhaka616@gmail.com>
2026-07-27 17:13:42 +0530

    practice: Construct a Full Binary Tree solution

---
+++ gfg/2026-07-27_construct-a-full-binary-tree.py
@@ -0,0 +1,7 @@
+# Problem   : Construct a Full Binary Tree
+# Difficulty: Medium
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-07-27
+# ───────────────────────────────────────────────────────
+# Time complexity: O(C_N * N) where C_N is the N-th Catalan number. More precisely, O(N * C_N) for generating all unique trees, and O(N^2 * C_N) if deep copying trees. Here
