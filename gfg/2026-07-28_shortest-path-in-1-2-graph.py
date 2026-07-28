477880ffb8c17d951847908b25255a367ae5965a
himesh026 <himeshdhaka616@gmail.com>
2026-07-28 21:24:09 +0530

    practice: Shortest Path in 1-2 Graph solution

---
+++ gfg/2026-07-28_shortest-path-in-1-2-graph.py
@@ -0,0 +1,14 @@
+# Problem   : Shortest Path in 1-2 Graph
+# Difficulty: Hard
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-07-28
+# ───────────────────────────────────────────────────────
+# O(V + E)
+# O(V + E)
+import collections
+import math
+
+def shortest_path_1_2(n, adj, start_node, end_node):
+    """
+    Finds the shortest path in a graph where
