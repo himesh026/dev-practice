b8aa27e668000745d32a24e53251e183268ec29c
himesh026 <himeshdhaka616@gmail.com>
2026-08-02 15:40:51 +0530

    feat: implement middleware utility

---
+++ backend/2026-08-02_service.js
@@ -0,0 +1,7 @@
+// Type      : Backend Utility
+// Date      : 2026-08-02
+// ───────────────────────────────────────────────────────
+/**
+ * Paginates a Mongoose query.
+ * @param {object} Model - The Mongoose Model to query (e.g., Mongoose.model('User')).
+ * @param {object} [query={}] - The Mongoose query object (e.g
