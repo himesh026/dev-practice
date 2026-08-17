10f7cc671ed95a70dbb6d6f6b8c05344fd239922
himesh026 <himeshdhaka616@gmail.com>
2026-08-17 19:35:29 +0530

    feat: implement middleware utility

---
+++ backend/2026-08-17_service.js
@@ -0,0 +1,9 @@
+// Type      : Backend Utility
+// Date      : 2026-08-17
+// ───────────────────────────────────────────────────────
+module.exports = {
+  /**
+   * @typedef {object} SuccessResponse
+   * @property {boolean} success - Indicates if the request was successful.
+   * @property {*} data - The payload of the response.
+   * @property {string} message -
