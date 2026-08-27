d902018e5fe3cf09fb820ba1cabb3a2dbe613075
himesh026 <himeshdhaka616@gmail.com>
2026-08-28 00:38:35 +0530

    feat: solve Minimum Cost Selection

---
+++ gfg/2026-08-27_minimum-cost-selection.java
@@ -0,0 +1,16 @@
+// Problem   : Minimum Cost Selection
+// Difficulty: Medium
+// Tags      : [object Object]
+// Language  : Java
+// Date      : 2026-08-27
+// ───────────────────────────────────────────────────────
+// O(N * SUM_VALUE)
+// O(SUM_VALUE)
+import java.util.Arrays;
+
+class Solution {
+
+    private static final int INF = Integer.MAX_VALUE;
+
+    /**
+     * Finds the minimum cost to select items such
