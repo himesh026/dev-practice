e4f3f5820e2b9773371abb00e3b34143b5b615fc
himesh026 <himeshdhaka616@gmail.com>
2026-08-29 01:44:04 +0530

    add: Count Subsequences Divisible by n (DSA)

---
+++ gfg/2026-08-28_count-subsequences-divisible-by-n.py
@@ -0,0 +1,13 @@
+# Problem   : Count Subsequences Divisible by n
+# Difficulty: Medium
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-08-28
+# ───────────────────────────────────────────────────────
+# O(len(nums) * n)
+# O(n)
+MOD = 10**9 + 7
+
+def count_subsequences_divisible_by_n(nums, n):
+    """
+    Counts the number of non-empty subsequences of
