ee107c873b72df4e234197d10538f179f0c67b18
himesh026 <himeshdhaka616@gmail.com>
2026-08-29 08:17:32 +0530

    feat: solve Count Subsequences Divisible by n

---
+++ gfg/2026-08-29_count-subsequences-divisible-by-n.js
@@ -0,0 +1,10 @@
+// Problem   : Count Subsequences Divisible by n
+// Difficulty: Medium
+// Tags      : [object Object]
+// Language  : JavaScript
+// Date      : 2026-08-29
+// ───────────────────────────────────────────────────────
+// O(N * M) time complexity, where N is the length of nums and M is n
+// O(M) space complexity, where M is n
+const countSubsequencesDivisibleByN = (nums, n) => {
+    // A large prime
