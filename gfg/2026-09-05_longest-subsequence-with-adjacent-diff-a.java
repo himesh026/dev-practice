501737babf3ee682943bd1d0d35068079308f561
himesh026 <himeshdhaka616@gmail.com>
2026-09-05 21:47:08 +0530

    practice: Longest Subsequence with Adjacent D solution

---
+++ gfg/2026-09-05_longest-subsequence-with-adjacent-diff-a.java
@@ -0,0 +1,8 @@
+// Problem   : Longest Subsequence with Adjacent Diff as 1
+// Difficulty: Medium
+// Tags      : [object Object]
+// Language  : Java
+// Date      : 2026-09-05
+// ───────────────────────────────────────────────────────
+// Time complexity: O(N) where N is the number of elements in the input array.
+// Space complexity: O(M) where M is the number of distinct elements in the input array (or O(max_val - min_val) if using an array
