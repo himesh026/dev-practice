f49fa75f2454ddc49d722b28223323109141c34c
himesh026 <himeshdhaka616@gmail.com>
2026-09-06 17:40:52 +0530

    feat: solve Sum of Pairwise ANDs

---
+++ gfg/2026-09-06_sum-of-pairwise-ands.java
@@ -0,0 +1,10 @@
+// Problem   : Sum of Pairwise ANDs
+// Difficulty: Medium
+// Tags      : [object Object]
+// Language  : Java
+// Date      : 2026-09-06
+// ───────────────────────────────────────────────────────
+// Time complexity: O(N * log(MAX_VAL)), where N is the number of elements and MAX_VAL is the maximum value an integer can hold (32 bits for int).
+// Space complexity: O(1)
+class Solution {
+    /**
