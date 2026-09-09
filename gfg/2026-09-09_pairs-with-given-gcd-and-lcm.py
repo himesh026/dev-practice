85e3437a2c70fc51f4e60024e686f8070e0edc61
himesh026 <himeshdhaka616@gmail.com>
2026-09-10 03:57:41 +0530

    practice: Pairs with Given GCD and LCM solution

---
+++ gfg/2026-09-09_pairs-with-given-gcd-and-lcm.py
@@ -0,0 +1,13 @@
+# Problem   : Pairs with Given GCD and LCM
+# Difficulty: Easy
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-09-09
+# ───────────────────────────────────────────────────────
+# O(sqrt(l) + log(l)) time complexity, where l is the LCM
+# O(1) space complexity
+import math
+
+def get_distinct_prime_factors_count(n: int) -> int:
+    """
+    Counts the
