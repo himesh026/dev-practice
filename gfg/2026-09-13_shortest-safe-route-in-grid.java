beb37f11af60531cb06aee6d6305338f78685b59
himesh026 <himeshdhaka616@gmail.com>
2026-09-14 03:50:31 +0530

    feat: solve Shortest Safe Route in Grid

---
+++ gfg/2026-09-13_shortest-safe-route-in-grid.java
@@ -0,0 +1,10 @@
+// Problem   : Shortest Safe Route in Grid
+// Difficulty: Medium
+// Tags      : [object Object]
+// Language  : Java
+// Date      : 2026-09-13
+// ───────────────────────────────────────────────────────
+// Time complexity: O(M*N) for pre-processing + O(M*N) for BFS = O(M*N)
+// Space complexity: O(M*N) for safeGrid, visited array, and queue = O(M*N)
+
+import java
