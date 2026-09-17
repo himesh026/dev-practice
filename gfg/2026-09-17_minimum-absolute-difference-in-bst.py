b5fb9a4696d8446d99520895219b3d6187ef533e
himesh026 <himeshdhaka616@gmail.com>
2026-09-18 04:18:53 +0530

    feat: solve Minimum Absolute Difference In BST

---
+++ gfg/2026-09-17_minimum-absolute-difference-in-bst.py
@@ -0,0 +1,14 @@
+# Problem   : Minimum Absolute Difference In BST
+# Difficulty: Medium
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-09-17
+# ───────────────────────────────────────────────────────
+O(N)
+O(H)
+
+class TreeNode:
+    """Definition for a binary tree node."""
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
