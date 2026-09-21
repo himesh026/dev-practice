f2b66b17f386ccc7984696877187ae284893cd49
himesh026 <himeshdhaka616@gmail.com>
2026-09-22 00:24:54 +0530

    practice: Longest Matching in Dictionary with solution

---
+++ gfg/2026-09-21_longest-matching-in-dictionary-with-remo.js
@@ -0,0 +1,8 @@
+// Problem   : Longest Matching in Dictionary with Removals
+// Difficulty: Medium
+// Tags      : [object Object]
+// Language  : JavaScript
+// Date      : 2026-09-21
+// ───────────────────────────────────────────────────────
+// Time complexity: O(M * N), where M is the number of words in the dictionary and N is the length of the string s.
+// Space complexity: O(L_max), where L_max is the maximum length of a word in the dictionary (for storing the result
