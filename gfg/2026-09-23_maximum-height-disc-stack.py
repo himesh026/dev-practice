523392db9143a4b918325542a5563f78c132f6f9
himesh026 <himeshdhaka616@gmail.com>
2026-09-24 04:23:21 +0530

    practice: Maximum Height Disc Stack solution

---
+++ gfg/2026-09-23_maximum-height-disc-stack.py
@@ -0,0 +1,9 @@
+# Problem   : Maximum Height Disc Stack
+# Difficulty: Hard
+# Tags      : [object Object]
+# Language  : Python
+# Date      : 2026-09-23
+# ───────────────────────────────────────────────────────
+# O(N log N) where N is 3 times the number of input discs (for rotations)
+# O(N) where N is 3 times the number of input discs (for rotations)
+def max_height_disc_stack(discs):
