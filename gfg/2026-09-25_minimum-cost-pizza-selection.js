4284e551e5e243cd9143341dffb2ad76ee068992
himesh026 <himeshdhaka616@gmail.com>
2026-09-26 04:44:15 +0530

    feat: solve Minimum Cost Pizza Selection

---
+++ gfg/2026-09-25_minimum-cost-pizza-selection.js
@@ -0,0 +1,9 @@
+// Problem   : Minimum Cost Pizza Selection
+// Difficulty: Medium
+// Tags      : [object Object]
+// Language  : JavaScript
+// Date      : 2026-09-25
+// ───────────────────────────────────────────────────────
+// Time complexity: O(N * 2^K), where N is the number of pizzas and K is the number of unique ingredients.
+// Space complexity: O(K + 2^K), where K is the number of unique ingredients.
+const solveMinimumCostPizza
