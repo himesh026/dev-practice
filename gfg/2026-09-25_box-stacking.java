d7259800cd4f44229c7264eb2832e4e451cab1bb
himesh026 <himeshdhaka616@gmail.com>
2026-09-25 23:36:16 +0530

    practice: Box Stacking solution

---
+++ gfg/2026-09-25_box-stacking.java
@@ -0,0 +1,8 @@
+// Problem   : Box Stacking
+// Difficulty: Hard
+// Tags      : [object Object]
+// Language  : Java
+// Date      : 2026-09-25
+// ───────────────────────────────────────────────────────
+// Time complexity: O(N^2), where N is the number of input boxes. This is because we generate 3N blocks and then perform a nested loop for DP, which is O((3N)^2) = O(N^2).
+// Space complexity
