40ae171c0d9a334af06640a694006680f603c0b9
himesh026 <himeshdhaka616@gmail.com>
2026-09-26 18:42:13 +0530

    feat: solve Minimum Cost Pizza Selection

---
+++ gfg/2026-09-26_minimum-cost-pizza-selection.js
@@ -0,0 +1,9 @@
+// Problem   : Minimum Cost Pizza Selection
+// Difficulty: Medium
+// Tags      : [object Object]
+// Language  : JavaScript
+// Date      : 2026-09-26
+// ───────────────────────────────────────────────────────
+// Time Complexity: O(N * (targetSlices + maxPizzaSlices)) where N is the number of pizzas.
+// Space Complexity: O(targetSlices + maxPizzaSlices)
+const minCostPizzaSelection = (pizzas, targetSlices
